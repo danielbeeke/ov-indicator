02e930a7b4b464a53ad36243f8d54922683b06a7
Daniel Beeke <mail@danielbeeke.nl>
2019-10-26 16:52:05 +0200

    Adding svg heart

--- public/javascript/BusSelector.js
+++ public/javascript/BusSelector.js
@@ -198,7 +198,7 @@ customElements.define('bus-selector', class BusSelector extends BaseElement {
         <button class="
           ${this.favoriteBusStops.includes(this.selectedBusStop.stop_id) ? 'favorite' : ''} 
           ${ this.animateToggleButtonBusStop ? 'show-animation' : '' } favorite-button" onclick="${this.toggleBusStopFavorite}">
-          <span class="inner">♥</span>
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 50 50" xml:space="preserve"><path d="M24.9 10.1c2-4.8 6.6-8.1 12-8.1 7.2 0 12.4 6.2 13.1 13.5 0 0 0.4 1.8-0.4 5.1 -1.1 4.5-3.5 8.5-6.9 11.5L24.9 48 7.4 32.2c-3.4-3-5.8-7-6.9-11.5 -0.8-3.3-0.4-5.1-0.4-5.1C0.7 8.2 5.9 2 13.2 2 18.5 2 22.8 5.3 24.9 10.1z" fill="#C03A2B"/><path d="M6 18.1c-0.6 0-1-0.4-1-1 0-5.5 4.5-10 10-10 0.6 0 1 0.4 1 1s-0.4 1-1 1c-4.4 0-8 3.6-8 8C7 17.6 6.6 18.1 6 18.1z" fill="#ED7161"/></svg>
         </button>
       ` : ''}
     </div>
@@ -220,7 +220,7 @@ customElements.define('bus-selector', class BusSelector extends BaseElement {
           ${this.favoriteTrips.includes(this.selectedTrip.route_short_name) ? 'favorite' : ''} 
           ${ this.animateToggleButtonTrip ? 'show-animation' : '' } favorite-button" 
           onclick="${this.toggleTripFavorite}">
-          <span class="inner">♥</span>
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 50 50" xml:space="preserve"><path d="M24.9 10.1c2-4.8 6.6-8.1 12-8.1 7.2 0 12.4 6.2 13.1 13.5 0 0 0.4 1.8-0.4 5.1 -1.1 4.5-3.5 8.5-6.9 11.5L24.9 48 7.4 32.2c-3.4-3-5.8-7-6.9-11.5 -0.8-3.3-0.4-5.1-0.4-5.1C0.7 8.2 5.9 2 13.2 2 18.5 2 22.8 5.3 24.9 10.1z" fill="#C03A2B"/><path d="M6 18.1c-0.6 0-1-0.4-1-1 0-5.5 4.5-10 10-10 0.6 0 1 0.4 1 1s-0.4 1-1 1c-4.4 0-8 3.6-8 8C7 17.6 6.6 18.1 6 18.1z" fill="#ED7161"/></svg>
         </button>
       </div>
     ` : ''} 
